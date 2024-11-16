e0b80df6e664cc8a7503639d4b598c1124a699ca
Akash Yadav <114010679+akash-lifegod@users.noreply.github.com>
2024-11-16 09:06:37 +0530

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // MONGOURI: 'mongodb+srv://govindnacharya:05082003@projectalexa.adyt8ry.mongodb.net/Users?retryWrites=true',
+    // MONGOURI: 'mongodb://localhost:27017'
+}
